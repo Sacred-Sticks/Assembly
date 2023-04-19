a059e326e5ed73789356fd235e2c994788a12325
Lucas Ackman <AckmanLD@gmail.com>
2023-04-19 10:42:56 -0500

    Updated Binary Conversion to use LINQ

--- Assembly Program/Assembly/Register.cs
+++ Assembly Program/Assembly/Register.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Assembly
 {
@@ -24,40 +26,73 @@ namespace Assembly
         public static int minValue { get; private set; }
         
         
+        // public static void ConvertToBinary(int value, out bool[] binaryValue)
+        // {
+        //     if (value < minValue || value > maxValue)
+        //     {
+        //         throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(value)} must be between {minValue} and {maxValue}");
+        //     }
+        //     int currentValue = value;
+        //     if (currentValue < 0)
+        //         currentValue *= -1;
+        //     binaryValue = new bool[BITS];
+        //     for (int i = 0; i < BITS - 1; i++)
+        //     {
+        //         binaryValue[i] = currentValue % 2 == 1;
+        //         currentValue /= 2;
+        //     }
+        //     if (value < 0)
+        //         binaryValue = LogicGates.Invert(binaryValue);
+        // }
+        //
+        // public static void ConvertToInteger(bool[] binaryValue, out int value)
+        // {
+        //     value = 0;
+        //     int multiplier = 1;
+        //     bool isNegative = binaryValue[BITS - 1];
+        //     if (binaryValue[BITS - 1])
+        //         binaryValue = LogicGates.Invert(binaryValue);
+        //     for (int i = 0; i < BITS - 1; i++)
+        //     {
+        //         if (binaryValue[i])
+        //             value += multiplier;
+        //         multiplier *= 2;
+        //     }
+        //     if (isNegative)
+        //         value *= -1;
+        // }
+        
+        
         public static void ConvertToBinary(int value, out bool[] binaryValue)
         {
             if (value < minValue || value > maxValue)
             {
                 throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(value)} must be between {minValue} and {maxValue}");
             }
-            int currentValue = value;
-            if (currentValue < 0)
-                currentValue *= -1;
-            binaryValue = new bool[BITS];
-            for (int i = 0; i < BITS - 1; i++)
-            {
-                binaryValue[i] = currentValue % 2 == 1;
+
+            int currentValue = value < 0 ? value - minValue : value;
+            binaryValue = Enumerable.Range(0, BITS)
+                .Select (i => {
+                bool bit = currentValue % 2 == 1;
                 currentValue /= 2;
-            }
-            if (value < 0)
-                binaryValue = LogicGates.Invert(binaryValue);
+                return bit;
+            })
+                .ToArray();
+            binaryValue[BITS - 1] = value < 0;
         }
 
-        public static void ConvertToInteger(bool[] binaryValue, out int value)
+        public static void ConvertToInteger(IEnumerable<bool> binaryValue, out int value)
         {
-            value = 0;
-            int multiplier = 1;
-            bool isNegative = binaryValue[BITS - 1];
-            if (binaryValue[BITS - 1])
-                binaryValue = LogicGates.Invert(binaryValue);
-            for (int i = 0; i < BITS - 1; i++)
-            {
-                if (binaryValue[i])
-                    value += multiplier;
-                multiplier *= 2;
-            }
+            bool[] binary = binaryValue as bool[] ?? binaryValue.ToArray();
+            bool isNegative = binary
+                .Last();
+
+            value = binary
+                .Take(binary.Count() - 1)
+                .Select((b, i) => (b ? 1 : 0) * (int)Math.Pow(2, i)).Sum();
+
             if (isNegative)
-                value *= -1;
+                value += minValue;
         }
     }
 }
